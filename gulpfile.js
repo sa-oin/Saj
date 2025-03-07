c8d976b6dd913ba9b7c95dd06ef0125cb761cae0
Ghost <essajayeevee@gmail.com>
2025-03-07 17:09:25 +0545

    Add package.json and gulpfile.js for build

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+const gulp = require('gulp');
+
+// Define the 'build' task
+gulp.task('build', function(done) {
+  console.log('Build task is running!');
+  // Add your build steps here
+  done(); // Signal task completion
+});
